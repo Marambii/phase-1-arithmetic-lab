5cb22301673a1f5618d48f5303664451d14a722a
Baruch Marambi Mwenda <marambib2019@gmail.com>
2023-03-14 15:53:18 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+Math.random();
+
+let num3 = 29
+let num4 = 5;
+let mod = num3 % num4
+
+let set = [5,10,20,9,6,4]
+let max = Math.max(...set)
